d2b472988e59e8bd080ed642e83fcf04d9f0ba1f
Rubab Rafique <rr@zerocall.com>
2012-06-15 15:42:33 +0500

    charge on telinta

--- apps/b2c/modules/customer/templates/smsHistorySuccess.php
+++ apps/b2c/modules/customer/templates/smsHistorySuccess.php
@@ -84,7 +84,7 @@
                 <tr>
                 	<td colspan="2" align="right"><strong><?php echo __('Subtotal') ?></strong></td>
                 	<!--
-                	<td><?php echo format_number($amount_total-$amount_total*.20) ?> dkk</td>
+                	<td><?php echo format_number($amount_total-$amount_total*.20) ?> NOK</td>
                 	 -->
                          <td><?php echo number_format($amount_total, 2, ',', '') ?> NOK</td>
                 </tr>
