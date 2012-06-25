477d83b7ea04c0fa3de091a314536d10e97c4ba8
khanmuhammad <khan.muhammad@zerocall.com>
2012-06-25 14:51:26 +0500

    paypal issue changes

--- apps/b2c/modules/customer/templates/_step_1.php
+++ apps/b2c/modules/customer/templates/_step_1.php
@@ -6,8 +6,8 @@
     <div class="split-form-sign-up">
         <div class="step-details"> 
             <strong><?php echo __('Become a Zapna Customer') ?> <span class="active">- <?php echo __('Step 1') ?>: <?php echo __('Register') ?> </span><span class="inactive">- <?php echo __('Step 2') ?>: <?php echo __('Payment') ?></span></strong>
-            <span style="margin-left:20px;"><img src="http://localhost/Norway/web/zerocall/images/siteseal.gif"></span>
-            <span style="margin-left: 5px"><img src="http://localhost/Norway/web/zerocall/images/ccs.png"></span>
+<!--            <span style="margin-left:20px;"><img src="http://localhost/Norway/web/zerocall/images/siteseal.gif"></span>
+            <span style="margin-left: 5px"><img src="http://localhost/Norway/web/zerocall/images/ccs.png"></span>-->
 <!--            <br><br><br><br>*--> <?php //echo __('Required to fill')?>  </div>
             <div class="fl col">
         <?php echo $form->renderHiddenFields() ?>
@@ -266,7 +266,7 @@
             </span>   
           </li>
           <li>
-              <input type="submit" class="butonsigninsmall1" name="submit" style="cursor: pointer; margin-left: 0px !important;"  value="<?php echo __('Process To Payment') ?>" />
+              <input type="submit" class="butonsigninsmall" name="submit" style="cursor: pointer; margin-left: 0px !important;"  value="<?php echo __('Process To Payment') ?>" />
           </li>
           </ul>
           <!-- end terms and conditions -->
