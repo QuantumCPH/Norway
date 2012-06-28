9613bface5f20957120cb95cc17bc0c48fe6e04f
Fatima Umer <fu@zerocall.com>
2012-06-28 16:45:16 +0500

    cus deactive

--- apps/backend/modules/customer/actions/actions.class.php
+++ apps/backend/modules/customer/actions/actions.class.php
@@ -148,9 +148,9 @@ public function executeDeActivateCustomer(sfWebRequest $request) {
                 $uniqueIdObj = UniqueIdsPeer::doSelectOne($uc);
                 $uniqueIdObj->setStatus(0);
                 $uniqueIdObj->setAssignedAt("0000-00-00 00:00:00");
-                $uniqueIdObj->save();
+                $uniqueIdObj->save();*/
                 $customer->setCustomerStatusId(5);
-                $customer->save();*/
+                $customer->save();
                 $response_text .= "Customer De-activated, Customer Id=" . $customer_id;
                 $response_text .= '<br/>';
 
