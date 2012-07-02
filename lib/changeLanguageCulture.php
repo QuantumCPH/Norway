4f934404bee9ba9390401994a45fff9319e1ac68
Rubab Rafique <rr@zerocall.com>
2012-07-02 17:53:08 +0500

    email language

--- lib/changeLanguageCulture.php
+++ lib/changeLanguageCulture.php
@@ -7,7 +7,46 @@ class changeLanguageCulture{
 
     public static function languageCulture($request,$clsObj)
     {
-        $clsObj->getUser()->setAttribute('activelanguage', $getCultue);
-    }    
+     
+        // Retrieve data from the user session with a default value
+        $activelanguage = $clsObj->getUser()->getAttribute('activelanguage', '');
+
+        //------------------------Set Culture - Code Add By - Ahtsham - ZeroCall - Against Feature#6.1
+        
+
+        if(sfConfig::get('sf_app')=='agent'){
+            $clsObj->getUser()->setCulture('en');
+            $getCultue = $clsObj->getUser()->getCulture();
+            // Store data in the user session
+                    $clsObj->getUser()->setAttribute('activelanguage', $getCultue);
+        }else{
+                              
+           if($request->getParameter('language')=='en'){
+               // echo 'Before'.$clsObj->getUser()->getCulture();
+                $clsObj->getUser()->setCulture('en');
+                $getCultue = $clsObj->getUser()->getCulture();
+                 // Store data in the user session
+                // echo ' GetCulture:'. $getCultue;
+                $clsObj->getUser()->setAttribute('activelanguage', $getCultue);
+
+            }else if($request->getParameter('language')=='no'){
+                $clsObj->getUser()->setCulture('no');
+                $getCultue = $clsObj->getUser()->getCulture();
+                 // Store data in the user session
+                $clsObj->getUser()->setAttribute('activelanguage', $getCultue);
+                //$languages = $request->getLanguages();
+               // $language = $request->getPreferredCulture(array('en', 'fr'));
+            }else{
+
+                $clsObj->getUser()->setCulture('no');
+                $getCultue = $clsObj->getUser()->getCulture();
+                // Store data in the user session
+                $clsObj->getUser()->setAttribute('activelanguage', $getCultue);
+
+                //----------------------End Code -------------------------------               
+            }
+        }
+        //-----------------------------------------
+      }
 }
 ?>
