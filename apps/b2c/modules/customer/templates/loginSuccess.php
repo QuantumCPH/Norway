6eca2b13c8c1a22fa248d0f0fa798af493e1a914
Rubab Rafique <rr@zerocall.com>
2012-07-03 13:58:51 +0500

    forget password by mobile number

--- apps/b2c/modules/customer/templates/loginSuccess.php
+++ apps/b2c/modules/customer/templates/loginSuccess.php
@@ -65,7 +65,7 @@
               $class = 'class="text1"';
           }
         ?>    
-	<p <?php echo $class;?>><?php echo __('Write e-mail address you used for registration.<br />Your password will be sent to you via this email.') ?></p>
+	<p <?php echo $class;?>><?php echo __('Write mobile number you used for registration.<br />Your password will be sent to you via this sms and email.') ?></p>
 	<input   class="input"  type="text" name="email" id="forgot_password_email" /><br />
 	<?php if ($sf_user->hasFlash('send_password_error_message')): ?>
 	<p style="color: red; margin:6px auto;"><?php echo $sf_user->getFlash('send_password_error_message') ?></p>
